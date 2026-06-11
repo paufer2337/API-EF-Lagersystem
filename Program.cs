bb5c0c93c351a677e57bec5743e92d68a255fdc5
PAO <paufer2337@gmail.com>
2026-06-11 13:26:14 +0200

    Fixed SwaggerUI as startup page when ctrl+click on localhost

--- Program.cs
+++ Program.cs
@@ -19,7 +19,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "StorageApi v1");
+            options.RoutePrefix = string.Empty;
+        });
     }
 
 app.UseHttpsRedirection();
